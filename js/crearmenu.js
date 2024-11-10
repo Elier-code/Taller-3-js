59f0e3c254a25f5c03c34aaa78fead71b169e853
Elier Narvaez <eliernarvaez93@gmail.com>
2024-11-10 00:09:36 -0500

    Probado para el resto de rutas

--- js/crearmenu.js
+++ js/crearmenu.js
@@ -1,17 +1,17 @@
 var opciones = [
-    ["Gestion Estudiantes","../html/gestionestudiantes.html"],
-    ["Gestion de Programas Academicos","../html/gestionprogramasacademicos.html"],
-    ["Gestion de Matriculas","../html/gestionmatriculas.html"],
+    ["Gestion Estudiantes","html/gestionestudiantes.html"],
+    ["Gestion de Programas Academicos","html/gestionprogramasacademicos.html"],
+    ["Gestion de Matriculas","html/gestionmatriculas.html"],
 ]
 var imagen
 function obtenerRutaImagen(rutaImagen){
-    var profundidad = window.location.pathname.split('/').length - 8
+    var profundidad = window.location.pathname.split('/').length - 7
     var ruta = ""
     for (var i = 0; i < profundidad; i++) {
         ruta += "../"
     }
     ruta += rutaImagen
-    console.log(profundidad)
+    console.log(ruta)
     var logo = document.createElement("img")
     logo.src = ruta
     logo.alt = "Logo de la Universidad"
